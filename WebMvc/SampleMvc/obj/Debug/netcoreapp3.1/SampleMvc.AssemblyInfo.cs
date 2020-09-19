f6e4a488dc4affb94f84f3107bead31c7bbe873b
Thiago Vaz <thiago.vaz@farfetch.com>
2020-09-19 09:50:50 +0100

    Add authentication to Web MVC

--- WebMvc/SampleMvc/obj/Debug/netcoreapp3.1/SampleMvc.AssemblyInfo.cs
+++ WebMvc/SampleMvc/obj/Debug/netcoreapp3.1/SampleMvc.AssemblyInfo.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-SampleMvc-D60EE84B-B47D-4995-B850-960B7880284B")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
